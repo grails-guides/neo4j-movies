5e2edce07dac37ddfa8b92a7aeedb6471f032c77
graemerocher <graeme.rocher@gmail.com>
2017-03-15 14:32:03 +0100

    Improve tests

--- complete/src/test/groovy/neo4j/movies/MovieServiceSpec.groovy
+++ complete/src/test/groovy/neo4j/movies/MovieServiceSpec.groovy
@@ -1,28 +1,35 @@
 package neo4j.movies
 
-import grails.test.neo4j.Neo4jSpec
+import grails.gorm.transactions.Rollback
 import org.grails.datastore.gorm.neo4j.Neo4jDatastore
+import spock.lang.AutoCleanup
+import spock.lang.Shared
+import spock.lang.Specification
 
 /**
  * See the API for {@link grails.test.mixin.services.ServiceUnitTestMixin} for usage instructions
  */
-class MovieServiceSpec extends Neo4jSpec {
+class MovieServiceSpec extends Specification {
 
-    MovieService service
+    @Shared @AutoCleanup Neo4jDatastore datastore = new Neo4jDatastore(getClass().getPackage())
+    @Shared MovieService service = datastore.getService(MovieService)
 
-    void setup() {
-        Neo4jDatastore neo4jDatastore = this.neo4jDatastore
-        service = neo4jDatastore.services.find { it instanceof MovieService }
-    }
-
+    @Rollback
     void "Test search movies"() {
+        given:
+        new Movie(title: "The Matrix", tagline: "Free your mind", released: 1999).save(flush:true)
+
         expect:"to find results"
         service.search("Matr")
         service.search("The")
         !service.search("The Wrestler")
     }
 
+    @Rollback
     void "Test find movie by title"() {
+        given:
+        new Movie(title: "The Matrix", tagline: "Free your mind", released: 1999).save(flush:true)
+
         expect:"The find movies by title"
         service.find("The Matrix")
         !service.find("The Wrestler")
